5e0a5f157623be4d4437849524c9957d7b7f5390
Sandip <sandip.pokharel@avishkaram.com>
2024-11-19 23:41:13 +0545

    example for using thread with callback

---
+++ CallbackThread/NumberHelper.cs
@@ -0,0 +1,37 @@
+﻿namespace CallbackThread
+{
+    //First creat the callback delegate with the same signature of the callback method
+    public delegate void ResultCallbackDelegate(int results);
+    
+    //Creating the helper class
+    public class NumberHelper
+    {
+        // Creating two private variables to hold the Number and ResultCallback instance
+        private int _number;
+        private ResultCallbackDelegate _resultCallbackDelegate;
+
+        //Initializing the private variables through constructor
+        //So while creating the instance you need to pass the value for Number and callback delegate
+        public NumberHelper(int number, ResultCallbackDelegate resultCallbackDelegate)
+        {
+            _number = number;
+            _resultCallbackDelegate = resultCallbackDelegate;
+        }
+
+        // This is the Thread function which will calculate the sum of the numbers
+        public void CalculateSum()
+        {
+            int result = 0;
+            for (int i = 1; i < _number; i++)
+            {
+                result += i;
+            }
+            Thread.Sleep(5000);
+            // Before the end of the thread function call the callback method
+            if (_resultCallbackDelegate != null)
+            {
+                _resultCallbackDelegate(result);
+            }
+        }
+    }
+}
---
+++ CallbackThread/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CallbackThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the ResultCallbackDelegate instance and to its constructor
+            // pass the callback method name
+            ResultCallbackDelegate resultCallbackDelegate = new ResultCallbackDelegate(ResultCallbackMethod);
+
+            int number = 20;
+
+            // Creating the instance of NumberHelper class by passing the Number
+            // the callback delegate instance
+            NumberHelper obj = new NumberHelper(number, resultCallbackDelegate);
+
+            // Creating the Thread using ThreadStart delegate
+            Thread t1 = new Thread(new ThreadStart(obj.CalculateSum));
+
+            t1.Start();
+
+            Console.Read();
+        }
+
+        // Callback method and the signature should be the same as the callback delegate signature
+        public static void ResultCallbackMethod(int Result)
+        {
+            Console.WriteLine("The Result is " + Result);
+        }
+    }
+}
