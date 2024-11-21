78a646c7603d72e56ddb53987b9a34644de4700d
Sandip <sandip.pokharel@avishkaram.com>
2024-11-21 23:45:35 +0545

    Thread Sychronization example

---
+++ ThreadSynchronization/BookMyShow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSynchronization
+{
+    internal class BookMyShow
+    {
+        private object lockObject = new object();
+
+        int availableTickets = 3;
+        static int I = 1, J = 2, K = 3;
+
+        public void BookTicket(string name, int wantedTickets)
+        {
+            lock (lockObject)
+            {
+                if (wantedTickets <= availableTickets)
+                {
+                    Console.WriteLine(wantedTickets + " booked to " + name);
+                    availableTickets -= wantedTickets;
+                }
+                else
+                {
+                    Console.WriteLine("No tickets available to book");
+                }
+            }
+        }
+        public void TicketBooking()
+        {
+            string name = Thread.CurrentThread.Name;
+
+            if (name.Equals("Thread 1"))
+            {
+                BookTicket(name, I);
+            }
+            else if (name.Equals("Thread 2"))
+            {
+                BookTicket(name, J);
+            }
+            else
+            {
+                BookTicket(name, K);
+            }
+        }
+    }
+}
---
+++ ThreadSynchronization/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace ThreadSynchronization
+{
+    class Program
+    {
+        static object LOCK_OBJECT = new object();
+        public static void Main(string[] args)
+        {
+            #region Thread Without Locking
+            //Thread t1 = new Thread(WithoutLockMethod);
+            //Thread t2 = new Thread(WithoutLockMethod);
+            //Thread t3 = new Thread(WithoutLockMethod);
+
+            //t1.Start();
+            //t2.Start();
+            //t3.Start();
+            #endregion
+
+            #region Thread With Locking
+            //Thread tl1 = new Thread(ThreadLockMethod)
+            //{
+            //    Name = "Thread 1"
+            //};
+
+            //Thread tl2 = new Thread(ThreadLockMethod)
+            //{
+            //    Name = "Thread 2"
+            //};
+
+            //Thread tl3 = new Thread(ThreadLockMethod)
+            //{
+            //    Name = "Thread 3"
+            //};
+
+            //tl1.Start();
+            //tl2.Start();
+            //tl3.Start();
+            #endregion
+
+            #region Ticket Booking Example With Thread Synchronous
+            BookMyShow bookShow = new BookMyShow();
+            Thread bs1 = new Thread(bookShow.TicketBooking)
+            {
+                Name = "Thread 1"
+            };
+            Thread bs2 = new Thread(bookShow.TicketBooking)
+            {
+                Name = "Thread 2"
+            };
+            Thread bs3 = new Thread(bookShow.TicketBooking)
+            {
+                Name = "Thread 3"
+            };
+
+            bs1.Start();
+            bs2.Start();
+            bs3.Start();
+            #endregion
+
+            Console.Read();
+        }
+        public static void WithoutLockMethod()
+        {
+            Console.Write("[Welcome To The ");
+            Thread.Sleep(1000);
+            Console.WriteLine("World of Dotnet!]");
+        }
+
+        public static void ThreadLockMethod()
+        {
+            // Locking the Shared Resource for Thread Synchronization
+            lock (LOCK_OBJECT)
+            {
+                Console.Write("[Welcome To The ");
+                Thread.Sleep(1000);
+                Console.WriteLine("World of Dotnet!]");
+            }
+        }
+    }
+}
