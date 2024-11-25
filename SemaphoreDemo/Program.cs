e779275d926dddb3f8c4fbf942d5a71408bbd934
Sandip <sandip.pokharel@avishkaram.com>
2024-11-26 01:11:32 +0545

    semaphore demo

--- SemaphoreDemo/Program.cs
+++ SemaphoreDemo/Program.cs
@@ -1,30 +1,69 @@
 ﻿namespace SemaphoreDemo
 {
+    //class Program
+    //{
+    //    public static Semaphore Semaphore { get; set; }
+    //    static void Main(string[] args)
+    //    {
+    //        try
+    //        {
+    //            //Try to Open the Semaphore if Exists, if not throw an exception
+    //            Semaphore = Semaphore.OpenExisting("SemaphoreDemo");
+    //        }
+    //        catch
+    //        {
+    //            //If Semaphore not Exists, create a semaphore instance
+    //            //Here Maximum 2 external threads can access the code at the same time
+    //            Semaphore = new Semaphore(2, 2, "SemaphoreDemo");
+    //        }
+
+    //        Console.WriteLine("External Thread Trying to Acquiring");
+    //        Semaphore.WaitOne();
+    //        //This section can be access by maximum two external threads: Start
+    //        Console.WriteLine("External Thread Acquired");
+    //        Console.ReadKey();
+    //        //This section can be access by maximum two external threads: End
+
+    //        Semaphore.Release();
+    //    }
+    //}
+
     class Program
     {
-        public static Semaphore Semaphore { get; set; }
+        public static Semaphore Semaphore = new Semaphore(2, 3);
+
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread threadObject = new Thread(DoSomeTask)
+                {
+                    Name = "Thread " + i
+                };
+                threadObject.Start();
+            }
+            Console.ReadKey();
+        }
+
+        static void DoSomeTask()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter the Critical Section for processing");
             try
             {
-                //Try to Open the Semaphore if Exists, if not throw an exception
-                Semaphore = Semaphore.OpenExisting("SemaphoreDemo");
+                //Blocks the current thread until the current WaitHandle receives a signal.
+                Semaphore.WaitOne();
+
+                //Decrease the Initial Count Variable by 1
+                Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Doing its work");
+                Thread.Sleep(5000);
+                Console.WriteLine(Thread.CurrentThread.Name + "Exit.");
             }
-            catch
+            finally
             {
-                //If Semaphore not Exists, create a semaphore instance
-                //Here Maximum 2 external threads can access the code at the same time
-                Semaphore = new Semaphore(2, 2, "SemaphoreDemo");
+                //Release() method to release semaphore  
+                //Increase the Initial Count Variable by 1
+                Semaphore.Release();
             }
-
-            Console.WriteLine("External Thread Trying to Acquiring");
-            Semaphore.WaitOne();
-            //This section can be access by maximum two external threads: Start
-            Console.WriteLine("External Thread Acquired");
-            Console.ReadKey();
-            //This section can be access by maximum two external threads: End
-
-            Semaphore.Release();
         }
     }
 }
