3f787fbf1796a6ecef25647e47ca5d4393e63d06
Sandip <sandip.pokharel@avishkaram.com>
2024-12-28 13:29:12 +0545

    method to insert data in array list

---
+++ Ex1_CreateArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace CreateArrayList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(1);
+            arrayList.Add("Hello");
+            arrayList.Add(3.33);
+            arrayList.Add(false);
+            arrayList.Add('c');
+            arrayList.Add(null);
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                Console.Write($"{arrayList[i]} ");
+            }
+
+            Console.ReadKey();
+
+            ArrayList arrayList2 = new ArrayList()
+            {
+                102, "World", true, 12.3, null
+            };
+
+            foreach (var data in arrayList2)
+            {
+                Console.WriteLine(data);
+            }
+            Console.ReadKey();
+        }
+    }
+}
