a1fe99acc1f2290e35eb34d2b825deae98bafe92
Sandip <sandip.pokharel@avishkaram.com>
2024-12-31 00:53:03 +0545

    create and remove data in hashtable

---
+++ Ex1-CreateAndAddElements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("UserId", 2002);
+        ht.Add("Name", "Don Dai");
+        ht.Add("Location", "USA");
+
+        Console.WriteLine("Printing Hashtable using Foreach Loop");
+        foreach (object o in ht.Keys)
+        {
+            Console.WriteLine(o + " : " + ht[o]);
+        }
+        //Or
+        //foreach (DictionaryEntry de in hashtable)
+        //{
+        //    Console.WriteLine($"Key: {de.Key}, Value: {de.Value}");
+        //}
+
+        Console.WriteLine("\nPrinting Hashtable using keys");
+        Console.WriteLine("Name : " + ht["Name"]);
+        Console.WriteLine("Location : " + ht["Location"]);
+
+        Console.ReadKey();
+
+        Hashtable citesHt = new Hashtable()
+        {
+            {"Nepal", "Kathmandu, Dolakha, Mustang" },
+            {"UK", "London, Manchester, Birmingham"}, //Key:UK, Value:London, Manchester, Birmingham
+            {"USA", "Chicago, New York, Washington"}, //Key:USA, Value:Chicago, New York, Washington
+            {"India", "Mumbai, New Delhi, Pune"}
+        };
+
+        Console.WriteLine("\nCreating a Hashtable Using Collection-Initializer\n");
+        foreach (DictionaryEntry city in citesHt)
+        {
+            Console.WriteLine($"Key: {city.Key}, Value: {city.Value}");
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ Ex2-CheckAvailabilityHashtable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Hashtable ht = new Hashtable();
+
+        ht.Add("UserId", 1001);
+        ht.Add("Name", "Don Sir");
+        ht.Add("Job", "No Any Job");
+        ht.Add("Salary", "Someone please give me");
+
+        //Checking the key using the Contains methid
+        Console.WriteLine("Is User Id Exists : " + ht.Contains("UserId"));
+        Console.WriteLine("Is EmailId Key Exists : " + ht.Contains("EmailId"));
+
+        //Checking the key using the ContainsKey method Id
+        Console.WriteLine("Is Job Key Exists : " + ht.ContainsKey("Job"));
+
+        //Checking the value using the ContainsValue method
+        Console.WriteLine("Is Mumbai value Exists : " + ht.ContainsValue("Don Sir"));
+
+        Console.ReadKey();
+    }
+}
