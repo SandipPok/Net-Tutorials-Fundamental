4ea6324a74c19d6322d22b01ae5eaddb58f8f9ea
Sandip <sandip.pokharel@avishkaram.com>
2024-12-31 00:56:45 +0545

    assign value in hashtable

---
+++ Ex4-AssignValueHashtable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Hashtable ht = new Hashtable();
+
+        ht[1] = "One";
+        ht[2] = 213;
+        ht[20] = false;
+
+        foreach (DictionaryEntry entry in ht)
+        {
+            Console.WriteLine($"Key: {entry.Key} and Value: {entry.Value}");
+        }
+
+        Console.ReadKey();
+    }
+}
