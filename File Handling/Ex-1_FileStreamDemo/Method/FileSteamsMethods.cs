b65c011948c8b3ac57ff863cc5de2989dbce9636
Sandip <sandip.pokharel@avishkaram.com>
2025-03-04 22:45:37 +0545

    update file streams example

---
+++ File Handling/Ex-1_FileStreamDemo/Method/FileSteamsMethods.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1_FileStreamDemo.Method
+{
+    internal class FileSteamsMethods
+    {
+        private readonly string FilePath = "";
+        public FileSteamsMethods(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void CreateNew()
+        {
+            try
+            {
+                // User to create a new file.
+                // If already exists it throws exception.
+                FileStream file = new FileStream(FilePath, FileMode.CreateNew, FileAccess.Write);
+                file.Close();
+                Console.WriteLine("File Created successfully");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void Create()
+        {
+            try
+            {
+                // User to create a new file.
+                // If already exists it replace.
+                FileStream file = new FileStream(FilePath, FileMode.Create, FileAccess.Write);
+                file.Close();
+                Console.WriteLine("File Created Successfully");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void Open()
+        {
+            try
+            {
+                // It will open the file
+                // if not found throws error
+                FileStream file = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+
+                string data;
+
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    data = sr.ReadToEnd();
+                }
+
+                file.Close();
+
+                Console.WriteLine("The content in the file is:\n" + data);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void OpenOrCreate()
+        {
+            try
+            {
+                FileStream file = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.Read);
+
+                string data;
+
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    data = sr.ReadToEnd();
+                }
+
+                file.Close();
+
+                Console.WriteLine("The content in the file is:\n" + data);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public void Append()
+        {
+            try
+            {
+                FileStream file = new FileStream(FilePath, FileMode.Append, FileAccess.Write);
+
+                byte[] data = Encoding.Default.GetBytes("This is the file created by Sandip Pokharel");
+
+                file.Write(data, 0, data.Length);
+
+                file.Close();
+                Console.WriteLine("Successfully saved file with data");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ File Handling/Ex-1_FileStreamDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using Ex_1_FileStreamDemo.Method;
+using System.Reflection.Emit;
+
+namespace FileStreamDemo;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string path = @"D:\DotNet\Net Tutorials\File Handling\Ex-1_FileStreamDemo\myFile.txt";
+        Console.WriteLine("Select the operation to perform");
+    TOP:
+        Console.WriteLine(@"
+1. CreateNew
+2. Create
+3. Open
+4. OpenOrCreate
+5. Append
+");
+
+        int val = Convert.ToInt16(Console.ReadLine());
+
+        FileSteamsMethods method = new FileSteamsMethods(path);
+
+        switch (val)
+        {
+            case 1:
+                method.CreateNew();
+                break;
+            case 2:
+                method.Create();
+                break;
+            case 3:
+                method.Open();
+                break;
+            case 4:
+                method.OpenOrCreate();
+                break;
+            case 5:
+                method.Append();
+                break;
+            default:
+                Console.WriteLine("Enter the valid number");
+                break;
+        }
+        Console.WriteLine("Do you want to continue (y or press any key) ? \n");
+
+        char ans = Convert.ToChar(Console.ReadLine());
+
+        if (ans == 'y' || ans == 'Y')
+            goto TOP;
+    }
+}
