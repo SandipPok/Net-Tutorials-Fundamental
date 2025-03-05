3f967355bad01684225c6129cf71b428d76c1d8b
Sandip <sandip.pokharel@avishkaram.com>
2025-03-06 01:48:56 +0545

    add method for write, writeline and read

---
+++ File Handling/Ex-2_StreamReaderWriterDemo/Method/StreamReaderWriter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamReaderWriterDemo.Method
+{
+    internal class StreamReaderWriter
+    {
+        private readonly string FilePath;
+
+        public StreamReaderWriter(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void Write()
+        {
+            StreamWriter sw = new StreamWriter(FilePath);
+            Console.WriteLine("Enter the text that you want to write: ");
+            string? input = Console.ReadLine();
+
+            sw.Write(input);
+
+            sw.Flush();
+            sw.Close();
+
+            Console.WriteLine("File Created Successfully");
+        }
+
+        public void WriteLine()
+        {
+            using (StreamWriter sw = new StreamWriter(FilePath))
+            {
+
+                Console.WriteLine("Enter the text that you want to write: ");
+                string? input = Console.ReadLine();
+
+                sw.WriteLine(input);
+            }
+
+            Console.WriteLine("File Created Successfully");
+        }
+
+        public void Open()
+        {
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
