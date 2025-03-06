0683082bcd9b2a8d0757bf733c48245e1d661c5f
SandipPok <67257506+SandipPok@users.noreply.github.com>
2025-03-06 12:45:30 +0545

    Update Program.cs
    add method to call seek and append

--- File Handling/Ex-2_StreamReaderWriterDemo/Program.cs
+++ File Handling/Ex-2_StreamReaderWriterDemo/Program.cs
@@ -7,11 +7,13 @@ namespace StreamReaderWriterDemo
         public static void Main(string[] args)
         {
             string path = @"D:\DotNet\Net Tutorials\File Handling\Ex-2_StreamReaderWriterDemo\myFile.txt";
+        LOOP:
             Console.WriteLine(@"
 1. Write
 2. WriteLine
 3. Open
-4. Append");
+4. Append
+5. Seek");
             Console.WriteLine("Enter the option to perform task");
 
             int option = Convert.ToInt16(Console.ReadLine());
@@ -30,12 +32,21 @@ namespace StreamReaderWriterDemo
                     srw.Open();
                     break;
                 case 4:
+                    srw.Append();
+                    break;
+                case 5:
+                    srw.Seek();
                     break;
                 default:
                     break;
             }
 
-            Console.ReadKey();
+            Console.WriteLine("Do you want to continue(y/n): ");
+
+            var choice = Convert.ToChar(Console.ReadLine());
+
+            if (choice == 'y')
+                goto LOOP;
         }
     }
 }
