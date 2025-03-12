52a4f5f23ba69a4ece8267e08d9263390239d031
Sandip <sandip.pokharel@avishkaram.com>
2025-03-13 02:12:19 +0545

    understand and use the async and await method

---
+++ Asynchronous Programming/Ex1-AsyncAwaitDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AsyncAwaitDemo;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Main thread started");
+
+        SomeMethodAsync();
+
+        Console.WriteLine("Main thread finished");
+        Console.ReadKey();
+    }
+    public static void SomeMethod()
+    {
+        Console.WriteLine("SomeMethod started");
+        Thread.Sleep(TimeSpan.FromSeconds(10));
+        Console.WriteLine("\n");
+        Console.WriteLine("SomeMethod finished");
+    }
+    public async static Task SomeMethodAsync()
+    {
+        Console.WriteLine("SomeMethodAsync started");
+        // await Task.Delay(TimeSpan.FromSeconds(10));
+        // Console.WriteLine("\n");
+        await Wait();
+        Console.WriteLine("SomeMethodAsync finished");
+    }
+
+    private async static Task Wait()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(10));
+        Console.WriteLine("\n10 Seconds wait Completed\n");
+    }
+}
