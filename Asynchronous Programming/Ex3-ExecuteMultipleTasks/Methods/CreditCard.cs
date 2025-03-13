03ddaabd7a389dba14db54641c2393cfb8780b64
Sandip <sandip.pokharel@avishkaram.com>
2025-03-14 00:45:59 +0545

    method for running with WhenAll, Task.Run and without any

---
+++ Asynchronous Programming/Ex3-ExecuteMultipleTasks/Methods/CreditCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_ExecuteMultipleTasks.Methods
+{
+    public class CreditCard
+    {
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+        public static List<CreditCard> GenerateCreditCards(int number)
+        {
+            List<CreditCard> creditCards = new List<CreditCard>();
+
+            for (int i = 0; i < number; i++)
+            {
+                creditCards.Add(new CreditCard
+                {
+                    CardNumber = "10000000" + i,
+                    Name = "CreditCard-" + i
+                });
+            }
+
+            return creditCards;
+        }
+    }
+}
---
+++ Asynchronous Programming/Ex3-ExecuteMultipleTasks/Methods/Method.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3_ExecuteMultipleTasks.Methods
+{
+    class Method
+    {
+        public static async void ProcessCreditCards(List<CreditCard> creditCards)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var tasks = new List<Task<string>>();
+
+            foreach (var creditCard in creditCards)
+            {
+                var response = ProcessCard(creditCard);
+                tasks.Add(response);
+            }
+
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            Console.WriteLine($"Processing of {creditCards.Count} Credit Cards Done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+        }
+        public static async void ProcessCreditCardsSync(List<CreditCard> creditCards)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            foreach (var creditCard in creditCards)
+            {
+                var response = await ProcessCard(creditCard);
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Processing of {creditCards.Count} Credit Cards Done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+        }
+
+        public static async void ProcessCreditCardsTaskRun(List<CreditCard> creditCards)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var tasks = new List<Task<string>>();
+
+            await Task.Run(() =>
+            {
+                foreach (var creditCard in creditCards)
+                {
+                    var response = ProcessCard(creditCard);
+                    tasks.Add(response);
+                }
+            });
+
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            Console.WriteLine($"Processing of {creditCards.Count} Credit Cards Done in {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+        }
+
+        public static async Task<string> ProcessCard(CreditCard creditCard)
+        {
+            await Task.Delay(1000);
+            string message = $"Credit Card Number: {creditCard.CardNumber} Name: {creditCard.Name} Processed";
+            Console.WriteLine($"Credit Card Number: {creditCard.CardNumber} Processed");
+            return message;
+        }
+    }
+}
