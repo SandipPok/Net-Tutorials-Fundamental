daf383ac8dca02585e1beb8e3dd0034c998f713c
Sandip <sandip.pokharel@avishkaram.com>
2025-03-14 02:03:26 +0545

    initial setup

---
+++ Asynchronous Programming/Ex4-LimitConcurrentTask/Methods/CreditCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4_LimitConcurrentTask.Methods
+{
+    class CreditCard
+    {
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+
+        public static List<CreditCard> GenerateCreditCards(int number)
+        {
+            List<CreditCard> creditCards = new List<CreditCard>();
+            for (int i = 0; i < number; i++)
+            {
+                creditCards.Add(new CreditCard
+                {
+                    CardNumber = "10000000" + i,
+                    Name = "CreditCard-" + i
+                });
+            }
+        }
+    }
+}
---
+++ Asynchronous Programming/Ex4-LimitConcurrentTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace LimitConcurrentTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("Main Thread Started");
+
+            Console.WriteLine("Main Thread Completed");
+            stopwatch.Stop();
+            Console.WriteLine($"Main Thread Execution Time {stopwatch.ElapsedMilliseconds / 1000.0} Seconds");
+            Console.ReadKey();
+        }
+    }
+}
