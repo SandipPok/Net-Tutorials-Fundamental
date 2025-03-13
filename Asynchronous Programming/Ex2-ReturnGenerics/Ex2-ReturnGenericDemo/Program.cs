c06ca67ab0d84cda9e4628dec23dd11f673ffc5a
Sandip <sandip.pokharel@avishkaram.com>
2025-03-13 15:19:43 +0545

    get the return type in async progaramming

--- Asynchronous Programming/Ex2-ReturnGenerics/Ex2-ReturnGenericDemo/Program.cs
+++ Asynchronous Programming/Ex2-ReturnGenerics/Ex2-ReturnGenericDemo/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace ReturnGenericDemo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Main Method Started...");
+        Console.WriteLine("Enter the Name: ");
+        string name = Console.ReadLine() ?? "Ram";
+
+        SomeMethod(name);
+
+        Console.WriteLine("Main Method End");
+        Console.ReadKey();
+    }
+
+    public async static void SomeMethod(string name)
+    {
+        Console.WriteLine("Some Method Started....");
+
+        try
+        {
+            var greetingMessage = await Greetings(name);
+            Console.WriteLine($"\n{greetingMessage}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"\nError Occurred. {ex.Message}");
+        }
+        Console.WriteLine("Some Method End");
+    }
+
+    private static async Task<string> Greetings(string name)
+    {
+        string message = string.Empty;
+        using (var client = new HttpClient())
+        {
+            client.BaseAddress = new Uri("http://localhost:5020");
+
+            HttpResponseMessage res = await client.GetAsync($"/greetings?name={name}");
+            message = await res.Content.ReadAsStringAsync();
+        }
+        return message;
+    }
+}
