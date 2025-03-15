3070aa1dd6f54f0b4b8ecec5b5659e0beaed6aa5
Sandip Pokharel <poksandip1@gmail.com>
2025-03-15 22:36:02 +0545

    create a retry pattern demo

---
+++ Asynchronous Programming/Ex7-RetryPattern/Pattern.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex7_RetryPattern
+{
+    class Pattern
+    {
+        public static async Task RetryMethod(int retryTimes = 3, int waitTime = 500)
+        {
+            for (int i = 0; i < retryTimes; i++)
+            {
+                try
+                {
+                    await RetryOperation();
+                    Console.WriteLine("Operation Successful");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Retry {i + 1}: Getting Exception : {ex.Message}");
+                    await Task.Delay(waitTime);
+                }
+            }
+        }
+
+        public static async Task RetryOperation()
+        {
+            await Task.Delay(500);
+            throw new Exception("Exception Occurred in while Processing...");
+        }
+    }
+}
