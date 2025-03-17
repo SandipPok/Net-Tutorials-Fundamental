0e5c65d5bba4e67b31744b601e08c800778be6c7
Sandip Pokharel <poksandip1@gmail.com>
2025-03-17 23:49:17 +0545

    demo for Control Result task in async programming

---
+++ Asynchronous Programming/Ex9-Control_Result_Task/MethodImpl.cs
@@ -0,0 +1,83 @@
+﻿using System.Threading.Tasks;
+
+namespace Ex9_Control_Result_Task
+{
+    class MethodImpl
+    {
+        public static async void ControlResultTask(string value)
+        {
+            var task = EvaluateValue(value);
+            Console.WriteLine("Method Started");
+            try
+            {
+                Console.WriteLine($"Is Completed: {task.IsCompleted}");
+                Console.WriteLine($"Is IsCanceled: {task.IsCanceled}");
+                Console.WriteLine($"Is IsFaulted: {task.IsFaulted}");
+                await task;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Method Completed");
+        }
+
+        public static async void ControlResultTaskWithReturnValue(string value)
+        {
+            var task = EvaluateValue(value, "2nd method");
+            Console.WriteLine("Method Started");
+            try
+            {
+                Console.WriteLine($"Is Completed: {task.IsCompleted}");
+                Console.WriteLine($"Is IsCanceled: {task.IsCanceled}");
+                Console.WriteLine($"Is IsFaulted: {task.IsFaulted}");
+                var result = await task;
+                Console.WriteLine($"Result: {result}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            Console.WriteLine("Method Completed");
+        }
+
+        private static Task EvaluateValue(string value)
+        {
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            if (value == "1")
+            {
+                tcs.SetResult(null);
+            }
+            else if (value == "2")
+            {
+                tcs.SetCanceled();
+            }
+            else
+            {
+                tcs.SetException(new ApplicationException($"Invalid Value: {value}"));
+            }
+
+            return tcs.Task;
+        }
+        private static Task<string> EvaluateValue(string value, string methodName)
+        {
+            var tcs = new TaskCompletionSource<string>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            if (value == "1")
+            {
+                tcs.SetResult("Task Completed");
+            }
+            else if (value == "2")
+            {
+                tcs.SetCanceled();
+            }
+            else
+            {
+                tcs.SetException(new ApplicationException($"Invalid Value: {value}"));
+            }
+
+            return tcs.Task;
+        }
+    }
+}
