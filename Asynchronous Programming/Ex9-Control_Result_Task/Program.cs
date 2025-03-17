deda87c17ae8de0f084f6e54216825c65bd43c95
Sandip Pokharel <poksandip1@gmail.com>
2025-03-17 23:49:17 +0545

    demo for Control Result task in async programming

---
+++ Asynchronous Programming/Ex9-Control_Result_Task/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex9_Control_Result_Task
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        LOOP:
+            Console.WriteLine(@"1. Control the Result of a Task
+2. TaskCompletionSource With Return Value");
+
+            Console.Write("Enter your choice: ");
+            int.TryParse(Console.ReadLine(), out int choice);
+            Console.WriteLine("Enter a number between 1 and 3");
+            switch (choice)
+            {
+                case 1:
+                    MethodImpl.ControlResultTask(Console.ReadLine() ?? "1");
+                    break;
+                case 2:
+                    MethodImpl.ControlResultTaskWithReturnValue(Console.ReadLine() ?? "1");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+            Console.Write("Do you want to continue? (y/n): ");
+            char.TryParse(Console.ReadLine(), out char c);
+            if (c == 'y')
+            {
+                goto LOOP;
+            }
+        }
+    }
+}
