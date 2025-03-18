0bec2cd52ff5bfa2da388eefe156c8c1de0bca7b
Sandip Pokharel <poksandip1@gmail.com>
2025-03-18 23:19:54 +0545

    initial setup

---
+++ Asynchronous Programming/Ex11-Chaining_Task_Using_Continuation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex11_Chaining_Task_Using_Continuation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        LOOP:
+            Console.WriteLine(@"1. Control the Result of a Task
+2. TaskCompletionSource With Return Value");
+
+            Console.Write("Enter your choice: ");
+            int.TryParse(Console.ReadLine(), out int choice);
+            switch (choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+            Console.Write("Do you want to continue? (y/n): ");
+            char.TryParse(Console.ReadLine(), out char c);
+            if (c == 'y')
+            {
+                goto LOOP;
+            }
+        }
+    }
+}
