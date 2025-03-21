34bda8f50dc197437491d936d457bbf604991ad7
Sandip Pokharel <poksandip1@gmail.com>
2025-03-21 23:18:45 +0545

    add method to deal with async streams

---
+++ Asynchronous Programming/Ex15-AsynchronousStreams/MethodImpl.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex15_AsynchronousStreams
+{
+    class MethodImpl
+    {
+        public static IEnumerable<string> GenerateNames()
+        {
+            yield return "John";
+            yield return "Doe";
+            yield return "Jane";
+        }
+
+        public static async IAsyncEnumerable<string> GenerateNamesAsync()
+        {
+            yield return "John";
+            yield return "Doe";
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            yield return "Jane";
+        }
+    }
+}
---
+++ Asynchronous Programming/Ex15-AsynchronousStreams/Program.cs
@@ -0,0 +1,43 @@
+﻿using Ex15_AsynchronousStreams;
+
+namespace Ex15_AsynchronousStreamsDemo
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+        LOOP:
+            Console.WriteLine(@"1. Yield Return Demo
+2. Asynchronous Steam Operations");
+
+            Console.Write("Enter your choice: ");
+            int.TryParse(Console.ReadLine(), out int choice);
+            switch (choice)
+            {
+                case 1:
+                    foreach (var name in MethodImpl.GenerateNames())
+                    {
+                        Console.Write(name + " ");
+                    }
+                    Console.WriteLine("\n");
+                    break;
+                case 2:
+                    await foreach (var item in MethodImpl.GenerateNamesAsync())
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine("\n");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+            Console.Write("Do you want to continue? (y/n): ");
+            char.TryParse(Console.ReadLine(), out char c);
+            if (c == 'y')
+            {
+                goto LOOP;
+            }
+        }
+    }
+}
