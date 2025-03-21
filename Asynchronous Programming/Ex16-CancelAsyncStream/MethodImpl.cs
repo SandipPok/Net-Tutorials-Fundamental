ab2b62317a6c805076873ac9e11510484244886b
Sandip Pokharel <poksandip1@gmail.com>
2025-03-22 01:21:25 +0545

    demo for cancellation task in asynchronous stream

---
+++ Asynchronous Programming/Ex16-CancelAsyncStream/MethodImpl.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Ex16_CancelAsyncStream
+{
+    class MethodImpl
+    {
+        public static async IAsyncEnumerable<string> GenerateNameAsync()
+        {
+            var names = new List<string> { "Alice", "Bob", "Charlie", "Diana", "Eve" };
+            yield return names[0];
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            yield return names[1];
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            yield return names[3];
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            yield return names[4];
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            yield return names[5];
+        }
+
+        public static async IAsyncEnumerable<string> GenerateNameAsync([EnumeratorCancellation] CancellationToken token = default)
+        {
+            var names = new List<string> { "Alice", "Bob", "Charlie", "Diana", "Eve" };
+            yield return names[0];
+            await Task.Delay(TimeSpan.FromSeconds(3), token);
+            yield return names[1];
+            await Task.Delay(TimeSpan.FromSeconds(3), token);
+            yield return names[3];
+            await Task.Delay(TimeSpan.FromSeconds(3), token);
+            yield return names[4];
+            await Task.Delay(TimeSpan.FromSeconds(3), token);
+            yield return names[5];
+        }
+    }
+}
---
+++ Asynchronous Programming/Ex16-CancelAsyncStream/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Ex16_CancelAsyncStream
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+        LOOP:
+            Console.WriteLine(@"1. Cancellation using break statement in the loop
+2. Scheduling Different Continuation Tasks");
+
+            Console.Write("Enter your choice: ");
+            int.TryParse(Console.ReadLine(), out int choice);
+            switch (choice)
+            {
+                case 1:
+                    await foreach (var name in MethodImpl.GenerateNameAsync())
+                    {
+                        Console.Write(name + " ");
+
+                        if (name == "Diana")
+                        {
+                            break;
+                        }
+                    }
+                    Console.WriteLine("\n");
+                    break;
+                case 2:
+                    var cts = new CancellationTokenSource();
+                    cts.CancelAfter(TimeSpan.FromSeconds(5));
+
+                    try
+                    {
+                        await foreach (var name in MethodImpl.GenerateNameAsync(cts.Token))
+                        {
+                            Console.Write(name + " ");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("\n" + ex.Message);
+                    }
+                    finally
+                    {
+                        cts.Dispose();
+                        cts = null;
+                    }
+                    Console.WriteLine("\n");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+            Console.Write("Do you want to continue? (y/n): ");
+            char.TryParse(Console.ReadLine(), out char c);
+            if (c == 'y')
+            {
+                goto LOOP;
+            }
+        }
+    }
+}
