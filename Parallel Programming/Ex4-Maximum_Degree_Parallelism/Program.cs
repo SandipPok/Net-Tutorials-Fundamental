959d261e14ddcdcd38ec3d48b1689e653be1e096
Sandip Pokharel <poksandip1@gmail.com>
2025-03-24 22:29:52 +0545

    efficient way to use ParallelOption

---
+++ Parallel Programming/Ex4-Maximum_Degree_Parallelism/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex4_Maximum_Degree_Parallelism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int processorCount = Environment.ProcessorCount;
+            Console.WriteLine($"Processor Count on this Machine: {processorCount}\n");
+
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = processorCount - 1
+            };
+
+            Parallel.For(1, 11, options, i =>
+            {
+                Thread.Sleep(1500);
+                Console.WriteLine($"Value of i = {i}, Thread = {Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+    }
+}
