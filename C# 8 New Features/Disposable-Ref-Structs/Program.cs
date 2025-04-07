7c32877ee6748d66f6910a2a99e16f814cbcdb7a
Sandip Pokharel <poksandip1@gmail.com>
2025-04-07 21:13:32 +0545

    demo for disposable ref struct in c# 8

---
+++ C# 8 New Features/Disposable-Ref-Structs/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        using var rect = new Rectangle(10, 20);
+        Console.WriteLine($"Area of Rectangle: {rect.GetArea()}");
+        Console.WriteLine("Exiting Main method");
+    }
+}
+ref struct Rectangle
+{
+    public double Height { get; set; }
+    public double Width { get; set; }
+    public Rectangle(double height, double width)
+    {
+        Height = height;
+        Width = width;
+    }
+    public double GetArea() => Height * Width;
+    public void Dispose() => Console.WriteLine("Dispose method called");
+}
