a92bc399340131cf5d6c459dfce27e1b71a6757f
Sandip Pokharel <poksandip1@gmail.com>
2025-04-09 00:07:12 +0545

    demo for null coalecing assignment operator

---
+++ C# 8 New Features/Null-Coalescing-Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<int>? numberList = null;
+        int? number = null;
+
+        numberList ??= new List<int>();
+
+        numberList.Add(number ??= 25);
+
+        numberList.Add(number ??= 50);
+
+        foreach (var item in numberList)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine("\n" + number);
+    }
+}
